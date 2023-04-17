d3f3dafb327463126395f7e924bf9a19725146c0
chris.kuo <chris.kuo@beyondcars.com>
2023-04-17 15:58:54 +0800

    SocketServer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const SocketServer = require('ws').Server;
 
 const PORT = process.env.PORT || 3005;
 
@@ -7,16 +8,40 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next()
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next()
 })
 
 app.get('/', (req, res) => {
-    res.send({ message: 'hello orange' })
+    res.send({ message: 'i love orange' })
 })
 
-app.listen(PORT, () => {
-    console.log(`API 服務啟動在端口 ${PORT}`)
+app.post('/print-qrcode', (req, res) => {
+  const clients = wss.clients;
+  clients.forEach(client => {
+    client.send(JSON.stringify(req.body))
+  })
+
+  res.send({ message: 'success' })
+})
+
+const server = app.listen(PORT, () => {
+  console.log(`API 服務啟動在端口 ${PORT}`)
+})
+
+const wss = new SocketServer({ server });
+
+wss.on('connection', ws => {
+  console.log('WebSocket 連接成功');
+
+  ws.on('message', data => {
+    console.log(JSON.parse(data));
+  })
+
+  ws.on('close', () => {
+    console.log('WebSocket 連接關閉');
+  })
+
 })
